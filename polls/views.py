a6915a302e58546627b9d8cbc2d152efd2463865
Gennady Terekhov <ter.ge@yandex.ru>
2021-01-05 18:42:04 +0300

    updated gitignore adn polls.views

--- polls/views.py
+++ polls/views.py
@@ -28,11 +28,9 @@ class ResultsView(generic.DetailView):
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
     context = {
         'latest_question_list': latest_question_list,
     }
-
     return render(request, 'polls/index.html', context)
 
 
